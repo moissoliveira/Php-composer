f906dcc6c3f2a2e3348818471bcc7ccd264641ee
Moises Oliveira <moiss.peol@gmail.com>
2020-07-30 10:43:38 -0400

    Upgrade de arquivos

---
+++ buscra-curso.php
@@ -0,0 +1,8 @@
+<?php
+
+use GuzzleHttp\client;
+
+$client = new Client();
+$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+
+$html = $resposta->getBody();
